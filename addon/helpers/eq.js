14787d93e003999e72f7817bf1dbd40b442c3991
James Conkling <james.lane.conkling@gmail.com>
2016-10-14 16:11:35 -0400

    make eq helper strong equals

--- addon/helpers/eq.js
+++ addon/helpers/eq.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
-export function eq(params/*, hash*/) {
-  return params[0] == params[1];
+export function eq(params) {
+  return params[0] === params[1];
 }
 
 export default Ember.Helper.helper(eq);
