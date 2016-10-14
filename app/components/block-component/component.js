aac4739bb8d71c02a825c2c574a693c16973d7b6
James Conkling <james.lane.conkling@gmail.com>
2016-10-14 17:08:30 -0400

    yeild message body in component dropdown content

---
+++ app/components/block-component/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-simple-typeahead/components/block-component/component';
---
+++ app/components/typeahead-list/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-simple-typeahead/components/typeahead-list/component';
